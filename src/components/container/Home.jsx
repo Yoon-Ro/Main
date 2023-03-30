a8d22833ddccde8894a6a07c58fd0bcaaa88b600
Yoon Ro <yoonro1995@gmail.com>
2023-03-30 17:11:21 +0900

    update name button with classic style

--- src/components/container/Home.jsx
+++ src/components/container/Home.jsx
@@ -1,37 +1,33 @@
 import React from "react";
 import hero from "../../assets/hero.png";
 import { logos } from "../../Data";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Home = () => {
- 
-  
-  
   return (
-    <div className="w-full sm:pt-0 pt-[6rem] sm:px-0 px-2 sm:w-[1420px] mx-auto" id="home">
+    <div
+      className="w-full sm:pt-0 pt-[6rem] sm:px-0 px-2 sm:w-[1420px] mx-auto"
+      id="home"
+    >
       <div className="md:flex  items-center  h-[100vh] sm:pt-0 pt-[4rem]">
         {/* HERO TEXT - LEFT */}
-        <div className="w-3/4">
-<h1 className="text-6xl font-medium px-8 py-2 my-4 bg-[#507efc] inline-block rounded-full text-white">Hi, I'm Yoon</h1>
-          <div className="sm:text-3xl  font-bold">
-          Experienced graphic designer based in Vancouver.<br/>-&gt;Graphic Design, UX Design, Front End Dev
-    
+        <div className="w-3/4 relative">
+          <div className="absolute bg-white/0 w-[20rem] h-10 z-10 rounded-full left-5 top-1 bg-gradient-to-b from-[#F1F7FE]" />
+          <div className="px-20 py-4  inline-block rounded-full bg-gradient-to-b from-[#50A2F0] from-0% via-[#A7DCFB] via-90% to-[#B7E5FC] to-100% relative shadow-xl shadow-blue-500/50">
+            <p className="text-4xl font-sm text-black/60 z-30">Hi, I'm Yoon</p>
+          </div>
+
+          <div className="sm:text-3xl  font-bold py-4">
+            Experienced graphic designer based in Vancouver.
+            <br />
+            -&gt;Graphic Design, UX Design, Front End Dev
           </div>
 
           {/* SOCIAL LINKS */}
-          
-
-
         </div>
-
-  
       </div>
 
-      <div>
-
-
-
-      </div>
+      <div></div>
     </div>
   );
 };
