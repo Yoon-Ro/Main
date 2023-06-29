5c534a053b13a0539a82dbd773bf1d75b4bd2b97
Yoon Ro <87830420+Yoon-Ro@users.noreply.github.com>
2023-06-29 11:39:52 -0700

    Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,15 +20,15 @@ const scrollActive = () => { setActive(window.scrollY > 20);};
   return (
     <div className={`${
       active ? "  " : ""
-    } fixed  inset-x-0 justify-center top-0 left-0 right-0 z-20`}>
-      <div>
+    } fixed  inset-x-0 justify-center top-0 left-0 right-0 z-20 `}>
+      <div >
         <div className={`${
-      active ? "py-4  " : "py-4"
-    } w-fit py-1 mx-auto flex items-center justify-center px-1 mt-8 bg-white/80 backdrop-blur  shadow-2xl rounded-full`}>
+      active ? "py-4" : "py-4 transition-all duration-125"} w-fit py-1 mx-auto flex items-center justify-center px-1 mt-8 bg-[#ededed]/70 backdrop-blur  shadow-2xl rounded-full `}>
 
-         <div className='flex items-center justify-between '>
-   <h1 className='font-medium text-lg rounded-full shadow-md px-4 py-3 bg-white'>Design</h1>
-   <h1 className='font-medium text-lg rounded-full px-4 py-3 text-[#0000007c]'><Link to="/me">About</Link></h1>
+         <div className='flex gap-4 items-center justify-between '>
+   <h1 className='text-sm rounded-full  px-8 py-1 bg-white'>Design</h1>
+   <h1 className=' text-sm rounded-full px-8 py-1 text-[#0000007c] hover:bg-blue-200 hover:text-blue-700 transition-all duration-100'><Link to="/me">About</Link></h1>
+   <h1 className=' text-sm rounded-full px-8 py-1 text-[#0000007c]'><Link to="/me">Resume</Link></h1>
          </div>         
 
 
