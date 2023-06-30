03f841a6b8a0863d3a89eb94ba7d7dd76ab854eb
Yoon Ro <87830420+Yoon-Ro@users.noreply.github.com>
2023-06-29 19:34:20 -0700

    Update Home

--- src/components/container/Home.jsx
+++ src/components/container/Home.jsx
@@ -7,18 +7,18 @@ import waving from "../../assets/waving.png";
 const Home = () => {
   return (
     <div
-      className="w-full sm:pt-0  sm:px-0 px-2  bg-gradient-to-b from-sky-400 to-sky-200"
+      className="w-full sm:pt-0  sm:px-0 px-2  bg-gradient-to-b from-slate-300 via-slate-200 to-white"
       id="home"
     ><div className="mx-auto">
       <div className="flex  items-center  h-[80vh] ">
         {/* HERO TEXT - LEFT */}
-        <div className="w-3/4 relative mx-auto">
-          <div className="absolute bg-white/0 w-[23rem] h-10 z-10 rounded-full left-5 top-1 bg-gradient-to-b from-[#F1F7FE]" />
-          <div className="px-20 py-4  inline-block rounded-full bg-gradient-to-b from-[#50A2F0] from-0% via-[#A7DCFB] via-90% to-[#B7E5FC] to-100% relative shadow-xl shadow-blue-500/50">
-            <p className=" text-[7rem] xs:text-4xl text-black/60 z-30 flex items-center gap-4">Hello there<img src={waving} alt="/" className="h-[44px] w-[44px]"/></p>
+        <div className="md:px-0 md:w-3/4 relative mx-auto">
+          <div className="absolute bg-white/0 w-[23rem] h-10 z-10 rounded-full left-[74px] md:left-5 top-1 bg-gradient-to-b from-[#F1F7FE]" />
+          <div className="left-14 md:left-0 px-20 py-4  inline-block rounded-full bg-gradient-to-b from-[#50A2F0] from-0% via-[#A7DCFB] via-90% to-[#B7E5FC] to-100% relative shadow-xl shadow-blue-600/40">
+            <p className=" text-[7rem] xs:text-4xl text-black z-30 flex items-center gap-4">Hello there<img src={waving} alt="/" className="h-[44px] w-[44px]"/></p>
           </div>
 
-          <div className="text-[2rem] leading-[3rem] py-8">
+          <div className="text-4xl py-16 md:text-xl md:py-8 font-light px-16 md:px-0 ">
           I’m Yoon, a digital graphic designer focused on            <br />
           empowering business and product growth with design.
           </div>
